26481265407608c7cbba6c7d97249f84c4cc19bb
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2014-11-21 15:45:27 -0500

    Added a working totorom unit test for consideration of moving to totorom from frames.
    Change-Id: Ic814df0eeabfaf4fed9e71d1ab410ccb257556a8

---
+++ src/test/java/com/syncleus/grail/graph/totorom/TotoromBasicTest.java
@@ -0,0 +1,47 @@
+package com.syncleus.grail.graph.totorom;
+
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
+import org.jglue.totorom.FrameFactory;
+import org.jglue.totorom.FramedGraph;
+import org.jglue.totorom.TypeResolver;
+import org.junit.Assert;
+
+public class TotoromBasicTest {
+    public void testBasic() {
+
+        Graph g = new TinkerGraph();
+        FramedGraph fg = new FramedGraph(g);
+        Person p1 = fg.addVertex(Person.class);
+        p1.setName("Bryn");
+
+        Person p2 = fg.addVertex(Person.class);
+        p2.setName("Julia");
+        Knows knows = p1.addKnows(p2);
+        knows.setYears(15);
+
+        Person bryn = fg.V().has("name", "Bryn").next(Person.class);
+
+
+        Assert.assertEquals("Bryn", bryn.getName());
+        Assert.assertEquals(15, bryn.getKnowsList().get(0).getYears());
+    }
+
+    public void testJavaTyping() {
+        Graph g = new TinkerGraph();
+        FramedGraph fg = new FramedGraph(g, FrameFactory.Default, TypeResolver.Java);//Java type resolver
+        //Also note FrameFactory.Default. Other options are CDI and Spring.
+
+        Person p1 = fg.addVertex(Programmer.class);
+        p1.setName("Bryn");
+
+        Person p2 = fg.addVertex(Person.class);
+        p2.setName("Julia");
+
+        Person bryn = fg.V().has("name", "Bryn").next(Person.class);
+        Person julia = fg.V().has("name", "Julia").next(Person.class);
+
+        Assert.assertEquals(Programmer.class, bryn.getClass());
+        Assert.assertEquals(Person.class, julia.getClass());
+    }
+}
